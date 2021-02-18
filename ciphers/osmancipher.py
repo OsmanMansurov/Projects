8bbd29ed86a406f75c2c9c5e3e8d0167e5976193
OsmanMansurov <ozzymansurov@gmail.com>
2021-02-18 12:48:47 -0700

    Added main function in custom cipher

--- ciphers/osmancipher.py
+++ ciphers/osmancipher.py
@@ -1,3 +1,29 @@
+def main():
+    while True:
+        inp = input("Would you like to encrypt, or decrypt? e/d: ")
+        if inp == "e":
+            plaintext = input("Plaintext: ")
+            a = int(input("Key A (must be coprime with 27): "))
+            b = int(input("Key B: "))
+            ciphertext = encrypt(plaintext, a, b)
+            print(f"Ciphertext: {ciphertext}")
+        elif inp == "d":
+            ciphertext = input("Ciphertext: ")
+            a = int(input("Key A (must be coprime with 27): "))
+            b = int(input("Key B: "))
+            plaintext = decrypt(ciphertext, a, b)
+            print(f"Plaintext: {plaintext}")
+        else:
+            print("Invalid input")
+        inp2 = input("Do you wish to exit? y/n: ")
+        if inp2 == "y" or inp2 == "Y":
+            break
+        elif inp2 == "n" or inp2 == "N":
+            continue
+        else:
+            print("Confused. Continuing loop.")
+
+
 def encrypt(plaintext, a, b):
     result = ""
     alphabetU = "QWERTYUIOPASDFGHJKLZXCVBNM"
@@ -54,3 +80,5 @@ def decrypt(ciphertext, a, b):
             result += i
     return result
 
+if __name__ == "__main__":
+    main()
